2410c78b504638423a54f7c04ed3fb90c6e4fe72
Inpatch <pietskov@proton.me>
2025-10-19 21:37:30 +0200

    alot

--- get_data.py
+++ get_data.py
@@ -48,9 +48,9 @@ class api_reddit_data():
         print(response.status_code)
         
 
-        file_name = f"{params["q"]}_{datetime.now().timestamp()}.json"
+        file_name = f"{params["q"]}_{self.subreddit}_{datetime.now().timestamp()}.json"
         # "data" is the folder name
-        file_path = os.path.join("data", file_name)
+        file_path = os.path.join("data_reddit_query", file_name)
         with open(file_path, "w") as file:
             json.dump(response.json(), file, indent=4)
         
@@ -76,7 +76,91 @@ class api_reddit_data():
 
 
 if __name__ == "__main__":
-    x = api_reddit_data("denmark")
-    x.get_data()
+
+# Source https://wiki.archiveteam.org/index.php/List_of_Reddit_subs_by_country_and_territory#Europe
+    reddit_urls = [
+    "https://old.reddit.com/r/europe/",
+    "https://old.reddit.com/r/AskEurope/",
+    "https://old.reddit.com/r/EuropeanCulture/",
+    "https://old.reddit.com/r/EuropeanUnion/",
+    "https://old.reddit.com/r/albania/",
+    "https://old.reddit.com/r/andorra/",
+    "https://old.reddit.com/r/austria/",
+    "https://old.reddit.com/r/belarus/",
+    "https://old.reddit.com/r/belgium/",
+    "https://old.reddit.com/r/bosnia/",
+    "https://old.reddit.com/r/bih/",
+    "https://old.reddit.com/r/bulgaria/",
+    "https://old.reddit.com/r/croatia/",
+    "https://old.reddit.com/r/cyprus/",
+    "https://old.reddit.com/r/czech/",
+    "https://old.reddit.com/r/de/",
+    "https://old.reddit.com/r/Denmark/",
+    "https://old.reddit.com/r/Eesti/",
+    "https://old.reddit.com/r/FaroeIslands/",
+    "https://old.reddit.com/r/Finland/",
+    "https://old.reddit.com/r/france/",
+    "https://old.reddit.com/r/germany/",
+    "https://old.reddit.com/r/gibraltar/",
+    "https://old.reddit.com/r/greece/",
+    "https://old.reddit.com/r/greenland/",
+    "https://old.reddit.com/r/hungary/",
+    "https://old.reddit.com/r/iceland/",
+    "https://old.reddit.com/r/ireland/",
+    "https://old.reddit.com/r/italy/",
+    "https://old.reddit.com/r/italia/",
+    "https://old.reddit.com/r/kosovo/",
+    "https://old.reddit.com/r/latvia/",
+    "https://old.reddit.com/r/liechtenstein/",
+    "https://old.reddit.com/r/lithuania/",
+    "https://old.reddit.com/r/luxembourg/",
+    "https://old.reddit.com/r/macedonia/",
+    "https://old.reddit.com/r/malta/",
+    "https://old.reddit.com/r/moldova/",
+    "https://old.reddit.com/r/Monaco/",
+    "https://old.reddit.com/r/montenegro/",
+    "https://old.reddit.com/r/thenetherlands/",
+    "https://old.reddit.com/r/nederlands/",
+    "https://old.reddit.com/r/norge/",
+    "https://old.reddit.com/r/norway/",
+    "https://old.reddit.com/r/Polska/",
+    "https://old.reddit.com/r/poland/",
+    "https://old.reddit.com/r/portugal/",
+    "https://old.reddit.com/r/romania/",
+    "https://old.reddit.com/r/rossiya/",
+    "https://old.reddit.com/r/roumania/",
+    "https://old.reddit.com/r/russia/",
+    "https://old.reddit.com/r/San_Marino/",
+    "https://old.reddit.com/r/serbia/",
+    "https://old.reddit.com/r/srbija/",
+    "https://old.reddit.com/r/slovakia/",
+    "https://old.reddit.com/r/slovenia/",
+    "https://old.reddit.com/r/spain/",
+    "https://old.reddit.com/r/espana/",
+    "https://old.reddit.com/r/sweden/",
+    "https://old.reddit.com/r/schweden/",
+    "https://old.reddit.com/r/switzerland/",
+    "https://old.reddit.com/r/suisse/",
+    "https://old.reddit.com/r/Transnistria/",
+    "https://old.reddit.com/r/transdniestria/",
+    "https://old.reddit.com/r/turkey/",
+    "https://old.reddit.com/r/Turkiye/",
+    "https://old.reddit.com/r/ukraine/",
+    "https://old.reddit.com/r/ukraina/",
+    "https://old.reddit.com/r/unitedkingdom/",
+    "https://old.reddit.com/r/Britain/",
+    "https://old.reddit.com/r/TheBritishIsles/",
+    "https://old.reddit.com/r/vatican/"
+]
+    subreddit_names = [url.split("/r/")[1].rstrip("/") for url in reddit_urls]
+    for i in subreddit_names:
+        x = api_reddit_data(i)
+    
+        for i in ["Chat Control", "CSAM"]:
+            x.get_data(query=i,sort="top",time="year")
+
+
+
+
 
 
